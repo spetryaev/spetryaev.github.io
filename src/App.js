0163c51e320952fc9d7be44af9e11e9df124bb33
Sergey Petryaev <sergey08rus@gmail.com>
2021-06-19 12:58:21 +0300

    Add sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import bcrypt from 'bcryptjs';
 
 const passwordHash = "$2a$10$isYv/GTTBnMGvpFUXHr6Eu44aox/r/rbE3nO4VOxKEj6LhnOwIs3K";
