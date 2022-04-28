4fffc4be4f3f378aa011bfb0c9207ae7c4a3d379
Sergey Petryaev <sergey08rus@gmail.com>
2022-04-29 00:35:46 +0300

    Edit webpage title

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,4 +1,9 @@
+import { useRef, useEffect } from 'react'
+import useDocumentTitle from '../utils/useDocumentTitle';
+
 function About() {
+    useDocumentTitle('About')
+    
     return (<div>Sergey Petryaev is ...</div>);
 }
 
